88cbc964af7b62e99fc48d1027b9569b9f28fb3a
Cornell T <trancornellh@gmail.com>
2020-12-09 15:06:40 -0800

    added some testing to pygame, basics

--- game.py
+++ game.py
@@ -1,2 +1,28 @@
 import pygame
 import sys
+
+pygame.init()
+
+window = pygame.display.set_mode((500, 500))
+pygame.display.set_caption("Placements")
+
+x = 50
+y = 50
+width = 50
+height = 50
+vel = 5
+
+running = True
+
+while running:
+    pygame.time.delay(100)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    pygame.draw.rect(window, (255, 0, 0), (x, y, width, height))
+    pygame.display.update()
+
+
+pygame.quit()
